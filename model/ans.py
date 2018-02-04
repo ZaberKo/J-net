eb171e620642f9f38c65943caac30a1115a0e3fb
ZaberKo <zaberchann@gmail.com>
2018-02-05 00:07:43 +0800

    save

--- model/ans.py
+++ model/ans.py
@@ -7,7 +7,7 @@ feature_emb_dim = 50
 hidden_dim = 150
 attention_dim = 150
 vocab_dim = 10000  # issue
-
+end_word = '</s>'
 question_seq_axis = C.Axis('questionAxis')
 passage_seq_axis = C.Axis('passageAxis')
 answer_seq_axis = C.Axis('answerAxis')
@@ -62,36 +62,50 @@ h_p1 = C.sequence.last(h_p)
 
 # decoder_initialization
 r = splice(C.slice(h_q1, 0, hidden_dim, 0), C.slice(h_q1, 0, hidden_dim, 0))
-d_0 = Dense(hidden_dim*2, activation=C.tanh, bias=True)(r)
-
-def output_layer(emb_word,att_p,att_q,hidden):
-    emb_word_ph=C.placeholder()
-    att_p_ph=C.placeholder()
-    att_q_ph=C.placeholder()
-    hidden_ph=C.placeholder()
-    return
-
-# d_0 = splice
-# print(d_0.shape)
-# answer_seq = C.sequence.delay(answer_seq)
+d_0 = Dense(hidden_dim * 2, activation=C.tanh, bias=True)(r)
 
 
-# @C.Function
-# def GRU__with_attention(hidden, x):
-#     p_att = p_attention(h_p, hidden)
-#     q_att = q_attention(h_q, hidden)
-#     x = splice(x, p_att, q_att)
-#     return gru(hidden, x)
+def output_layer(emb_word, att_p, att_q, hidden):
+    emb_word_ph = C.placeholder()
+    att_p_ph = C.placeholder()
+    att_q_ph = C.placeholder()
+    hidden_ph = C.placeholder()
+    readout = C.plus(
+        Dense(vocab_dim)(emb_word_ph),
+        Dense(vocab_dim)(att_q_ph),
+        Dense(vocab_dim)(att_p_ph),
+        Dense(vocab_dim)(hidden_ph)
+    )
+    word = C.argmax(C.softmax(readout))  # todo
+    return C.as_block(
+        word,
+        [(emb_word_ph, emb_word), (att_p_ph, att_p), (att_q_ph, att_q), (hidden_ph, hidden)]
+
+    )
 
 
-
+@C.Function
 def decoder(word_prev, hidden_prev, att_p_prev, att_q_prev):
     emb = emb_layer(word_prev)
     x = C.splice(word_prev, att_p_prev, att_q_prev)
     hidden = decoder_gru(hidden_prev, x)
     att_p = p_attention_layer(h_p, hidden)
     att_q = q_attention_layer(h_q, hidden)
-    word=output_layer()
+    word = output_layer(emb, att_p, att_q, hidden)
+    return C.combine([word, hidden, att_p, att_q])
+
+
+def model_factory():
+    answer_generator = UnfoldFrom(decoder,
+                                  until_predicate=
+                                  lambda word, _1, _2, _3: 1 if word == end_word else 0,
+                                  name="answer_generator"
+                                  )
+    # (initial_state, dynamic_axes_like)
+    word_0
+    att_p_0
+    att_q_0
+    answer_generator()
 
 #
 # UnfoldFrom(decoder)
